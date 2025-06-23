fd29f36e7e205686e601dca5a67df4cc1659807f
limavilam <limavilam@hotmail.com>
2025-06-23 15:17:57 -0500

    Exercises to improve my logic

---
+++ src/EjerciciosAlgoritmosSENA/PalindromoNumeros.java
@@ -0,0 +1,35 @@
+package EjerciciosAlgoritmosSENA;
+
+import java.util.Scanner;
+
+public class PalindromoNumeros {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese el número que quiere verificar si es palíndromo:\n");
+        int numero = scanner.nextInt();
+        int numeroOrginal = numero;
+
+        int numerofinal= 0;
+
+        while (numero != 0) {
+            int digito = numero % 10;
+            System.out.println(digito);
+            //iteración 1 = número final es 0
+            //iteracion 2 = número final + digito
+            numerofinal = numerofinal * 10 + digito;
+            System.out.println("El número final es:" + numerofinal);
+            numero = numero / 10;
+        }
+
+        if (numeroOrginal == numerofinal) {
+            System.out.println("El número ingresado: " + numeroOrginal + "es palíndromo");
+        }else{
+            System.out.println("El número ingresado no es palíndromo");
+        }
+
+
+
+
+    }
+}
--- src/EjerciciosAlgoritmosSENA/PalindromoPalabras.java
+++ src/EjerciciosAlgoritmosSENA/PalindromoPalabras.java
@@ -4,7 +4,7 @@ package EjerciciosAlgoritmosSENA;
 
 import java.util.Scanner;
 
-public class Palindromo {
+public class PalindromoPalabras {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
